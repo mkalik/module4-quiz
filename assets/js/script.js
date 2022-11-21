a17a89f64984c97d131f46943e68833402cd33a3
mkalik <mbadiankouyate@gmail.com>
2022-11-20 20:08:25 -0800

    finsihed up with game functionality, just need to add questions as well as styling. Also need to add some comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //script for quiz
 var clock = document.querySelector(".timer");
-var time = 6;
+var time;
 var check;
 function timer() {
   clock.textContent = time;
@@ -11,11 +11,17 @@ function countdown() {
   clock.textContent--;
   if (time <= 0) {
     clock.textContent = " ";
-    quiz.setAttribute("style", "display: none");
+
     start.style.display = "block";
     q_num = 0;
     time = 6;
+    console.log(game_start);
     clearInterval(check);
+    if (game_start) {
+      console.log("game_end");
+      start.setAttribute("style", "display:none");
+      game_end();
+    }
   }
 }
 var start = document.querySelector(".start");
@@ -68,35 +74,81 @@ function new_choices(size) {
 
   return choices;
 }
-var quiz = document.querySelector(".game");
+var quiz = document.querySelector(".quiz");
 var scores = document.querySelector(".view-scores");
 var q_keys = Object.values(problems);
-var q_num = 0;
 var u_right = document.querySelector(".correct");
 var options = document.querySelector(".options").children;
+var multiple_choice = document.querySelector(".options");
 var question = document.querySelector(".ask");
 var answers = Array.from(options);
-var score = 0;
+var quiz_end = document.querySelector(".log-scores");
+var all_scores = document.querySelector(".all-scores");
+var l_scores = document.querySelector(".l_score");
+var q_num;
+var score;
 var picks;
 var choices;
-document.querySelector(".options").addEventListener("click", function (event) {
+var game_start = false;
+var user_name;
+// var quiz_options = document.querySelector(".quiz-options");
+
+start.addEventListener("click", function (event) {
+  event.preventDefault();
+  start.setAttribute("style", "display: none");
+
+  game();
+  timer();
+});
+
+all_scores.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (event.target.value == "restart") {
+    game();
+    timer();
+    console.log("local storage size: " + localStorage.length);
+    all_scores.setAttribute("style", "display:none");
+  }
+  if (event.target.value == "clear") {
+    while (l_scores.firstChild) {
+      l_scores.removeChild(l_scores.firstChild);
+    }
+
+    localStorage.clear();
+
+    console.log("local storage size: " + localStorage.length);
+    alert("scores cleared");
+    all_scores.setAttribute("style", "display:none");
+    start.setAttribute("style", "display:block");
+  }
+});
+
+multiple_choice.addEventListener("click", function (event) {
   event.preventDefault();
   if (event.target.dataset.datatrue == "1") {
     console.log("correct chosen");
     score += 1;
-    q_num++;
     u_right.textContent = "Correct!";
     u_right.setAttribute("style", "display:block");
-    game_questions();
   } else {
     console.log("false");
     u_right.textContent = "Wrong!";
     u_right.setAttribute("style", "display:block");
     score -= 1;
-    q_num++;
+  }
+  q_num++;
+  if (q_num == q_keys.length) {
+    game_end();
+  } else {
+    game_questions();
   }
 });
+
 function game_questions() {
+  console.log("q_keys size: " + q_keys.length, "q_num: " + q_num);
+  if (q_num == q_keys.length) {
+    game_end();
+  }
   question.textContent = q_keys[q_num].ask;
   picks = new_choices(answers.length);
   console.log("picks: " + picks);
@@ -109,15 +161,42 @@ function game_questions() {
   }
 }
 function game() {
+  // quiz_options.setAttribute("style", "display:none");
+  u_right.setAttribute("style", "display:none");
+  all_scores.setAttribute("style", "display:none");
   console.log("game start");
+  score = 0;
+  time = 10;
+  q_num = 0;
+  game_start = true;
   quiz.setAttribute("style", "display: block");
   game_questions();
 }
-start.addEventListener("click", function (event) {
-  console.log(q_keys);
-  event.preventDefault();
-  start.setAttribute("style", "display: none");
-  q_num = 0;
-  timer();
-  game();
-});
+function game_end() {
+  time = 0;
+  quiz.setAttribute("style", "display: none");
+  quiz_end.setAttribute("style", "display:block");
+  console.log("score: " + score);
+  quiz_end.children[1].textContent = "your score: " + score;
+}
+function log_user() {
+  quiz_end.setAttribute("style", "display:none");
+  user_name = document.forms["info_form"]["user_name"].value;
+  console.log(user_name);
+  localStorage.setItem(user_name, score);
+  view_scores();
+}
+function new_score(elements) {
+  var new_score = document.createElement("li");
+  var l_name = localStorage.key(elements);
+  new_score.textContent = l_name + "   " + localStorage.getItem(l_name);
+  l_scores.appendChild(new_score);
+}
+function view_scores() {
+  var local_size = localStorage.length;
+  console.log("local_size: " + local_size);
+  for (var i = 0; i < local_size; i++) {
+    new_score(i);
+  }
+  all_scores.setAttribute("style", "display:block");
+}
